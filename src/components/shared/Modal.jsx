2b1ada78557b8a3ae7adca1e858e3cce691d0d8d
Johnny9Rodriguez <mail@joepytlik.de>
2024-08-19 14:10:33 +0200

    refactor: include isLoading for Modal behavior

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -1,21 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, cloneElement } from 'react';
 import useStaticDataStore from '../../stores/staticDataStore';
 import useOnClickOutside from '../../hooks/useCloseOnClickOutside';
 
-function CreateSceneModal({ children }) {
+function Modal({ children }) {
     const { closeModal } = useStaticDataStore();
+    const [isLoading, setIsLoading] = useState(false);
     const modalRef = useRef(null);
 
-    useOnClickOutside(modalRef, closeModal);
+    useOnClickOutside(modalRef, () => {
+        if (isLoading) return;
+        closeModal();
+    });
 
     return (
         <div
             className='p-4 mt-20 mb-auto w-52 flex flex-col items-center justify-center gap-4 bg-gradient-to-b from-gray-800 to-gray-900 border border-gray-700'
             ref={modalRef}
         >
-            {children}
+            {React.Children.map(children, (child) =>
+                cloneElement(child, { isLoading, setIsLoading })
+            )}
         </div>
     );
 }
 
-export default CreateSceneModal;
+export default Modal;
