546f514b2ca94b8d91d79e1b972fad60fb9d12bd
Johnny9Rodriguez <mail@joepytlik.de>
2024-09-27 12:38:38 +0200

    feat: add settings footer links

--- src/components/settings-page/FooterInfo.jsx
+++ src/components/settings-page/FooterInfo.jsx
@@ -6,14 +6,29 @@ import { IoMdMail } from 'react-icons/io';
 function FooterInfo() {
     const btnClasses = 'text-lg hover:cursor-pointer hover:text-white';
 
+    const openWebsite = () => {
+        window.huemidi.settings.openWebsite();
+    };
+
+    const openMail = () => {
+        window.huemidi.settings.openMail();
+    };
+
+    const openGithub = () => {
+        window.huemidi.settings.openGithub();
+    };
+
     return (
         <div className='flex gap-2 justify-end text-sm text-gray-400'>
-            <div className='flex gap-1 items-center hover:cursor-pointer hover:text-white'>
+            <div
+                className='flex gap-1 items-center hover:cursor-pointer hover:text-white'
+                onClick={openWebsite}
+            >
                 <MdCopyright className='text-base' />
                 <div>Joe Pytlik, 2024</div>
             </div>
-            <IoMdMail className={btnClasses} />
-            <IoLogoGithub className={btnClasses} />
+            <IoMdMail className={btnClasses} onClick={openMail} />
+            <IoLogoGithub className={btnClasses} onClick={openGithub} />
         </div>
     );
 }
